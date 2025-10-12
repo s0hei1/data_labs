e7a66949d787ff98cc2749e1b38d99bd2edbe659
Soheil <hesarisoheil1999@example.com>
2025-10-12 14:48:50 +0330

    some changes

---
+++ financial_markets/0002_predictor_simple/patterns_generator.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+#
+# class PatternGenerator:
+#
+#
+#     def generate_n_len_pattern_by_single_col(
+#             self,
+#             df : pd.DataFrame,
+#             id_col_name : str = 'timestamp',
+#             pattern_
+#             n : int = 7
+#     ) -> pd.DataFrame:
+#         pass
